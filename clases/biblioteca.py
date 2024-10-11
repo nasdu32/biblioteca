3eb956c01a11bd64e12d6ad6177cee88f8e7662d
ignacio <ignacio.hdulol@gmail.com>
2024-10-11 00:07:30 -0300

    trabajo UML

--- clases/biblioteca.py
+++ clases/biblioteca.py
@@ -1,18 +1,22 @@
 from miembro import Miembro
+from libro import Libro
 
 class Biblioteca:
-    def init(self, nombre):
+    def init(self, nombre, libros):
+        
         self._nombre = nombre
-        self._libros = []
+        self._libros = libros
         self._personas = []
 
     def agregar_persona(self, persona):
         self._personas.append(persona)
 
     def buscar_libro(self, titulo):
+        
         return next((libro for libro in self._libros if libro.titulo == titulo), None)
 
     def prestar_libro(self, titulo, id_miembro):
+        
         libro = self.buscar_libro(titulo)
         miembro = next((p for p in self._personas if isinstance(p, Miembro) and p._id == id_miembro), None)
         if libro and miembro:
@@ -20,6 +24,7 @@ class Biblioteca:
         return False
 
     def devolver_libro(self, titulo, id_miembro):
+        
         libro = self.buscar_libro(titulo)
         miembro = next((p for p in self._personas if isinstance(p, Miembro) and p._id == id_miembro), None)
         if libro and miembro:
